8b5df26c9c3ea8024288436ab461df9da3f767ce
nagaganesh244 <nagaganesh244@gmail.com>
2025-06-25 19:56:30 +0530

    Add files via upload

---
+++ PUBLIC/SRC/data/data.js
@@ -0,0 +1,75 @@
+import img1 from './1.jpg';
+import img2 from './2.jpg';
+import img3 from './3.jpg';
+import img4 from './4.jpg';
+import img5 from './5.jpg';
+import img6 from './6.jpeg';
+import img7 from './7.jpg';
+import img8 from './8.jpg';
+import img9 from './9.jpg';
+import img10 from './10.jpeg';
+import img11 from './11.jpeg';
+import img12 from './12.jpeg';
+import img13 from './13.jpeg';
+import img14 from './14.jpeg';
+import img15 from './15.jpeg';
+import img16 from './16.jpeg';
+import img17 from './17.jpeg';
+import img18 from './18.jpeg';
+import img19 from './19.jpeg';
+import img20 from './20.jpg';
+import img21 from './21.jpg';
+import img22 from './22.jpg';
+import img23 from './23.jpg';
+import img24 from './24.jpeg';
+import img25 from './25.jpeg';
+import img26 from './26.jpeg';
+import img27 from './27.jpeg';
+import img28 from './28.jpg';
+import img29 from './29.jpg';
+import img30 from './30.jpg';
+import img31 from './31.jpg';
+import img32 from './32.jpg';
+import img33 from './33.jpg';
+import img34 from './34.jpg';
+import img35 from './35.jpg';
+import img36 from './36.jpg';
+
+export const videos = [
+  { thumbnail_id: 1, name: "INFINITY WAR", Channel: "Catherin Slyford", resolution: "4K", created_at: "2023-01-14 04:12:54", views: 8222249, likes: 96795, url: img1, tag: "movie" , saved: false  , liked: false}, 
+  { thumbnail_id: 2, name: "STAR WARS", Channel: "Bryn Littledike", resolution: "1080p", created_at: "2021-02-20 04:20:40", views: 2274543, likes: 17765, url: img2 , tag: "movie", saved: false , liked: false},
+  { thumbnail_id: 3, name: "PRIEST", Channel: "My Sauvan", resolution: "720p", created_at: "2018-05-08 17:18:51", views: 5115940, likes: 95380, url: img3 , tag: "movie", saved: false , liked: false},
+  { thumbnail_id: 4, name: "BLACK SEA", Channel: "Moreen Storres", resolution: "1080p", created_at: "2019-12-06 15:24:37", views: 142858, likes: 89227, url: img4 , tag: "movie", saved: false , liked: false},
+  { thumbnail_id: 5, name: "HELL BOY ", Channel: "Janine Wilshaw", resolution: "1080p", created_at: "2021-02-05 03:52:37", views: 4026819, likes: 78643, url: img5 , tag: "movie", saved: false , liked: false},
+  { thumbnail_id: 6, name: "SKYFALL", Channel: "Deeanne Orton", resolution: "1080p", created_at: "2022-05-23 13:06:02", views: 1806226, likes: 49792, url: img6 , tag: "trending", saved: false , liked: false},
+  { thumbnail_id: 7, name: "BLUE", Channel: "Trefor Huncote", resolution: "1080p", created_at: "2018-09-11 03:09:51", views: 2551860, likes: 86881, url: img7 , tag: "song", saved: false , liked: false},
+  { thumbnail_id: 8, name: "DIE WITH A SMILE", Channel: "Corenda Schneidar", resolution: "720p", created_at: "2023-05-30 00:21:00", views: 6321805, likes: 58640, url: img8 , tag: "song", saved: false , liked: false},
+  { thumbnail_id: 9, name: "HUKUM", Channel: "Shawn Sainte Paul", resolution: "480p", created_at: "2021-10-27 05:35:05", views: 5540763, likes: 96551, url: img9 , tag: "trending", saved: false , liked: false},
+  { thumbnail_id: 10, name: "SPIDERMAN: NO WAY HOME", Channel: "Cherlyn Dargue", resolution: "480p", created_at: "2019-12-02 15:58:34", views: 4734561, likes: 70354, url: img10 , tag: "movie", saved: false , liked: false},
+  { thumbnail_id: 11, name: "TRIPE 9", Channel: "Carin Oylett", resolution: "1080p", created_at: "2020-11-23 16:08:01", views: 103929, likes: 46875, url: img11 , tag: "movie", saved: false , liked: false},
+  { thumbnail_id: 12, name: "TITANIC", Channel: "Gweneth Walkden", resolution: "480p", created_at: "2021-09-16 07:12:56", views: 3951760, likes: 73464, url: img12 , tag: "trending", saved: false , liked: false},
+  { thumbnail_id: 13, name: "SHUTTER ISLAND", Channel: "Taber Shopcott", resolution: "4K", created_at: "2022-01-28 20:35:10", views: 3377795, likes: 16132, url: img13 , tag: "movie", saved: false , liked: false},
+  { thumbnail_id: 14, name: "BLACK ADAM", Channel: "Leshia Enrigo", resolution: "720p", created_at: "2023-02-21 22:59:14", views: 4999648, likes: 82638, url: img14 , tag: "movie", saved: false , liked: false},
+  { thumbnail_id: 15, name: "ANIMAL", Channel: "Marys Rosenberg", resolution: "4K", created_at: "2023-01-22 22:55:22", views: 7393251, likes: 54790, url: img15 , tag: "movie", saved: false , liked: false},
+  { thumbnail_id: 16, name: "ZOMBIELAND", Channel: "Ettore Sainer", resolution: "4K", created_at: "2022-03-24 07:39:13", views: 722988, likes: 16745, url: img16 , tag: "movie", saved: false , liked: false},
+  { thumbnail_id: 17, name: "KARATE KID", Channel: "Gamaliel Martyntsev", resolution: "480p", created_at: "2022-12-04 19:26:35", views: 1505713, likes: 82394, url: img17 , tag: "trending", saved: false , liked: false},
+  { thumbnail_id: 18, name: "TRANSFORMERS", Channel: "Drew MacKimmie", resolution: "720p", created_at: "2018-04-29 15:59:28", views: 4693255, likes: 63270, url: img18 , tag: "movie", saved: false , liked: false},
+  { thumbnail_id: 19, name: "RRR", Channel: "Valentin Leatherborrow", resolution: "1080p", created_at: "2021-06-22 03:09:13", views: 2469629, likes: 44214, url: img19 , tag: "movie", saved: false , liked: false},
+  { thumbnail_id: 20, name: "SAPPHIRE", Channel: "Darby Hedde", resolution: "1080p", created_at: "2021-12-01 17:43:14", views: 319291, likes: 25476, url: img20  , tag: "song", saved: false , liked: false},
+  { thumbnail_id: 21, name: "LOVE ME LIKE YOU DO", Channel: "Dawna Goom", resolution: "1080p", created_at: "2023-11-24 08:35:02", views: 1849897, likes: 55463, url: img21  , tag: "song", saved: false , liked: false},
+  { thumbnail_id: 22, name: "BEAST", Channel: "Clovis Cockley", resolution: "480p", created_at: "2021-06-16 03:59:49", views: 2383683, likes: 27683, url: img22  , tag: "trending", saved: false , liked: false},
+  { thumbnail_id: 23, name: "HUNTER", Channel: "Opaline Tidcomb", resolution: "1080p", created_at: "2018-04-18 17:55:15", views: 6569210, likes: 31159, url: img23  , tag: "song", saved: false , liked: false},
+  { thumbnail_id: 24, name: "JOKER", Channel: "Della Have", resolution: "1080p", created_at: "2023-04-05 08:34:19", views: 2057161, likes: 42636, url: img24  , tag: "movie", saved: false , liked: false},
+  { thumbnail_id: 25, name: "ENDGAME", Channel: "Gayle Lillgard", resolution: "4K", created_at: "2023-06-27 19:57:21", views: 4922907, likes: 45283, url: img25  , tag: "trending", saved: false , liked: false},
+  { thumbnail_id: 26, name: "BRAVE", Channel: "Mathian Giblett", resolution: "4K", created_at: "2018-10-12 05:36:57", views: 9173980, likes: 2875, url: img26  , tag: "trending", saved: false , liked: false},
+  { thumbnail_id: 27, name: "IRON MAN 3", Channel: "Archibaldo Tour", resolution: "4K", created_at: "2021-08-19 16:59:30", views: 4087641, likes: 64021, url: img27  , tag: "movie", saved: false , liked: false},
+  { thumbnail_id: 28, name: "JURASSIC WORLD", Channel: "Guinna Simony", resolution: "720p", created_at: "2021-12-18 01:30:26", views: 9894159, likes: 67096, url: img28  , tag: "movie", saved: false , liked: false},
+  { thumbnail_id: 29, name: "NIGHTMARE", Channel: "Marshall Anthoney", resolution: "720p", created_at: "2023-02-25 03:03:29", views: 3336838, likes: 32022, url: img29  , tag: "movie", saved: false , liked: false},
+  { thumbnail_id: 30, name: "SECRET LEVEL", Channel: "Rollins Antwis", resolution: "4K", created_at: "2018-03-21 18:31:49", views: 7158428, likes: 43655, url: img30  , tag: "trending", saved: false , liked: false},
+  { thumbnail_id: 31, name: "POWERHOUSE", Channel: "Damiano Dionisetti", resolution: "720p", created_at: "2020-09-09 03:03:34", views: 7754298, likes: 72071, url: img31  , tag: "song", saved: false , liked: false},
+  { thumbnail_id: 32, name: "BELIEVER", Channel: "Garrek Smails", resolution: "480p", created_at: "2023-01-19 10:58:38", views: 7732363, likes: 78751, url: img32 , tag: "trending" , saved: false , liked: false},
+  { thumbnail_id: 33, name: "SARKARURA", Channel: "Eugenia McCaughen", resolution: "480p", created_at: "2021-08-09 13:06:36", views: 1550775, likes: 17376, url: img33  , tag: "song", saved: false , liked: false},
+  { thumbnail_id: 34, name: "SQUID GAME", Channel: "Anna-diane Copland", resolution: "4K", created_at: "2017-11-05 05:16:39", views: 4312441, likes: 16108, url: img34  , tag: "movie", saved: false , liked: false},
+  { thumbnail_id: 35, name: "THE LAST OF US", Channel: "Kippie Dulinty", resolution: "1080p", created_at: "2020-09-22 09:51:10", views: 9931686, likes: 63681, url: img35   , tag: "movie", saved: false , liked: false},
+  { thumbnail_id: 36, name: "WICKED", Channel: "Cinnamon Peatman", resolution: "480p", created_at: "2019-01-22 08:24:11", views: 8110803, likes: 64247, url: img36 , tag: "trending", saved: false , liked: false} 
+];
